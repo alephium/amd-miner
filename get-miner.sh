89ead22b8200adab49ef1fdd6417fc907f4a7ee2
Filipe Fortunato <7966765+capito27@users.noreply.github.com>
2021-12-02 15:08:19 +0100

    Cmake based build system, builds on windows

--- get-miner.sh
+++ get-miner.sh
@@ -2,13 +2,19 @@
 
 set -eu
 
-echo "Installing build-essential libuv"
-sudo apt install -y build-essential libuv1-dev opencl-headers
+echo "Installing build-essential cmake"
+sudo apt install -y build-essential cmake
 
-echo "Git cloning gpu-miner"
+echo "Git cloning amd-miner"
 git clone https://github.com/alephium/amd-miner.git
 
-echo "Building the gpu miner"
-cd amd-miner && make linux-gpu
+echo "Installing conan"
+temp_file=$(mktemp --suffix=.deb)
+curl -L https://github.com/conan-io/conan/releases/latest/download/conan-ubuntu-64.deb -o $temp_file
+sudo apt install "$temp_file"
+rm -f "$temp_file"
+
+echo "Building the amd miner"
+cd amd-miner && ./make.sh
 
 echo "Your miner is built, you could run it with: amd-miner/run-miner.sh"
